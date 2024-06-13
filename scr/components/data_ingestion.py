49abcc02b30f711540a8c3b8c35b31e5900b6161
hsdfhjkh d@ksdfghk .fdshsdk <dsadsfgjhgdsf@g>
2024-06-13 10:54:53 +0530

    pickle file added

--- scr/components/data_ingestion.py
+++ scr/components/data_ingestion.py
@@ -5,7 +5,7 @@ from scr.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass 
-
+from scr.components.data_transformation import DataTransformation
 
 ## Initialize the Data Ingestion Configuration
 
@@ -17,7 +17,7 @@ class DataIngestionconfig:
 
 ## create a class for data ingestion
 class DataIngestion:
-    def __init__(self) -> None:
+    def __init__(self):
         self.ingestion_config=DataIngestionconfig()
 
     def intitiate_data_ingestion(self):
@@ -27,7 +27,7 @@ class DataIngestion:
             df=pd.read_csv('notebook/data/gemstone.csv')
             logging.info('Dataset read as pandas dataframe')
 
-            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path))
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False)
             logging.info('Train test split')
             train_set,test_set = train_test_split(df,test_size=0.30,random_state=35)
@@ -54,4 +54,6 @@ class DataIngestion:
 
 if __name__ == '__main__':
     obj = DataIngestion()
-    train_data , test_data = obj.intitiate_data_ingestion()
+    train_data_path , test_data_path = obj.intitiate_data_ingestion()
+    data_transformation =DataTransformation()
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data_path,test_data_path)
